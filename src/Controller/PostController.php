ad2e6b516a25d8e1f8795e63d62a2f16f23e5bca
tonym <tony.marques@live.fr>
2024-02-29 21:28:30 +0100

    PostController > Add createPost

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
 use App\Repository\SessionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class PostController extends AbstractController
 {
-    #[Route('/posts', name: 'app_post')]
+    #[Route('/posts', name: 'app_post', methods: ["GET"])]
     public function index(
         PostRepository      $postRepository,
         SerializerInterface $serializer,
@@ -35,10 +37,46 @@ class PostController extends AbstractController
             return new JsonResponse('accès refusé', 401);
         }
 
-        $posts = $postRepository->findAll();
+        $posts = $postRepository->findBy([], ['createdAt' => "DESC"]);
 
         $jsonPosts = $serializer->serialize($posts, "json", ['groups' => "post:read"]);
 
         return new JsonResponse($jsonPosts, 200, [], true);
+    }
+
+    #[Route("/posts", methods: ['POST'])]
+    public function createPost(
+        PostRepository         $postRepository,
+        SerializerInterface    $serializer,
+        Request                $request,
+        SessionRepository      $sessionRepository,
+        EntityManagerInterface $em
+    )
+    {
+        $sessionCookie = str_replace("sessionId=", "", $request->headers->get("cookie"));
+
+        if (!$sessionCookie) {
+
+            return new JsonResponse('accès refusé', 401);
+        }
+
+        $session = $sessionRepository->findOneBy(["session" => $sessionCookie]);
+
+        if (!$session) {
+            return new JsonResponse('accès refusé', 401);
+        }
+
+        $user = $session->getUser();
+
+        $content = $request->getContent();
+
+        $post = $serializer->deserialize($content, Post::class, 'json');
+        $post->setUser($user)
+            ->setCreatedAt(new \DateTimeImmutable());
+
+        $em->persist($post);
+        $em->flush();
+
+        return $this->json($post, Response::HTTP_CREATED, [], ["groups" => "post:read"]);
     }
 }
